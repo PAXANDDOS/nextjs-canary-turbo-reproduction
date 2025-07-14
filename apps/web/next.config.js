0e46bfab3a0c3898f6328bec76b88bedf30e5fce
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2025-07-15 00:18:09 +0200

    update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // R:\test\apps\docs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    // transpilePackages: ['@repo/ui'],
+    // outputFileTracingRoot: path.resolve(__dirname, '../../'), // R:\test
+    turbopack: { root: path.resolve(__dirname, '../../') }, // R:\test
+};
 
 export default nextConfig;
